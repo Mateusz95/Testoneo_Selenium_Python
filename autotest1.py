6c1ed9b5cf368c1affff876ebe46c990b64b76bc
Mateusz Grzegrzółka <grzegmat95@gmail.com>
2020-01-07 20:06:49 +0100

    Add first test - check header text on login page

--- autotest1.py
+++ autotest1.py
@@ -1,7 +1,22 @@
 import unittest
+
 from selenium import webdriver
+
 
 class MainTests(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         self.driver = webdriver.Chrome(executable_path='D:\TestyAutomatyczne\chromedriver.exe')
+
+    def test_header_login_page(self):
+        driver = self.driver
+        url = 'https://autodemo.testoneo.com/en/login?back=my-account'
+        driver.get(url)
+        header_check = driver.find_element_by_xpath('//*[@id="main"]/header/h1')
+        header_text = header_check.text
+        expected_header = 'Log in to your account'
+        self.assertEqual(expected_header, header_text, f'Expected different header on page {url}')
+
+    @classmethod
+    def tearDownClass(self):
+        self.driver.quit()
